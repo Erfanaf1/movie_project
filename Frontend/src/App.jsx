db0583a710e84681f874c08122c1230bc5bb3647
Abol <abolfazlnasibi2024@gmail.com>
2026-05-07 17:47:36 +0330

    installed Vite + Tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-blue-500">
+        🎬 Movie Collection
+      </h1>
+    </div>
+  )
+}
+
+export default App
