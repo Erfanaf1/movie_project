03baabaecb7a97cbc211be9c06639ba25cf858bc
Abol <abolfazlnasibi2024@gmail.com>
2026-05-07 18:06:13 +0330

    setted up primary & surface colors

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen p-8">
-      <h1 className="text-3xl font-bold text-blue-500">
+    <div className="bg-surface-850 text-white min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-primary-300">
         🎬 Movie Collection
       </h1>
+      <button className="mt-8 px-4 py-2 bg-primary-300 text-surface-850 rounded-2xl font-medium">nigga move</button>
     </div>
   )
 }
