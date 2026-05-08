1f77ffa2d231cfbeba83981c15debfa687e14936
Abol <abolfazlnasibi2024@gmail.com>
2026-05-08 19:58:16 +0330

    prepared header components & setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <div className="bg-surface-850 min-h-screen px-12 py-8">
-      
+      <Header />
     </div>
   );
 }
