87550a95be7be274e81cbb9d054376d2674f5546
Abol <abolfazlnasibi2024@gmail.com>
2026-05-08 20:06:50 +0330

    fixed unused warning color to yellow

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import NavLinks from './NavLinks'
 
 const Header = () => {
   return (
---
+++ Frontend/src/components/header/NavLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavItem from './NavItem'
+
+const NavLinks = () => {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default NavLinks
