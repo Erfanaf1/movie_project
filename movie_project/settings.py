604553a6709a5a1a37947c62d0d1a108a3cf3f93
Erfan <Erfanaslefalah@gmail.com>
2026-05-08 09:35:13 +0330

    Database_preparation

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i-_xrt@9e(#oiln%3!zmyb1wy^_40a236-jxao-mi5p!csbh(c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.173.144.194', '192.168.1.83', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies'
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'movie_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -115,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/erfan/django-projects/movie_project/Backend/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=100)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    rating = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
